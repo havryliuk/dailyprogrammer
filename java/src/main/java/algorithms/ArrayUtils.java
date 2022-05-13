f68e736c42d644b1087d50a8250dec28522738a2
Oleksandr Gavryliuk <oleksandr.gavryliuk@gmail.com>
2022-05-13 15:58:46 +0300

    get even numbers from and reverse array

--- java/src/main/java/algorithms/ArrayUtils.java
+++ java/src/main/java/algorithms/ArrayUtils.java
@@ -1,7 +1,12 @@
 package algorithms;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.OptionalInt;
+import java.util.function.IntPredicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ArrayUtils {
 
@@ -25,5 +30,19 @@ public class ArrayUtils {
         }
 
         return false;
+    }
+
+    static int[] getEvenNumbers(int[] arrayA, int[] arrayB) {
+        IntPredicate isEven = value -> value % 2 == 0;
+        return Stream.of(arrayA, arrayB).flatMapToInt(Arrays::stream).filter(isEven).toArray();
+    }
+
+    static void reverse(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            int secondIndex = array.length - 1 - i;
+            array[i] = array[secondIndex];
+            array[secondIndex] = temp;
+        }
     }
 }

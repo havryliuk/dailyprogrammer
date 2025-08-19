2391b7ff63edc89473b208bae445c8f35b15669a
Oleksandr Gavryliuk <oleksander.havryliuk@gmail.com>
2025-08-19 17:11:20 +0300

    correct the test, add requirements.txt, correct the github actions script

--- python/advanced/test/test_doc_strings.py
+++ python/advanced/test/test_doc_strings.py
@@ -2,4 +2,9 @@ from advanced.doc_strings import my_function
 
 def test_doc_string():
     result = my_function.__doc__
-    assert "dfda" == result
+    assert """Print arguments.
+
+Parameters:
+arg1: first argument
+arg2: second argument. Defaults to None.
+""" == result
